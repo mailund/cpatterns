86aa5894183119e307c4ef6fef84197f03c54253
Thomas Mailund <mailund@birc.au.dk>
2018-12-02 21:30:49 +0100

    more loop/iterator patterns

--- iterators/iterators.c
+++ iterators/iterators.c
@@ -9,7 +9,7 @@ typedef int (*report_callback)(FILE *outfile, const char *str, ...);
 // Is it possible to make a 'yield' macro?
 
 
-#pragma simple loops
+#pragma mark simple loops
 
 void loop(FILE *outfile)
 {
@@ -18,10 +18,9 @@ void loop(FILE *outfile)
     }
 }
 
-void loop_cb(
-    FILE *outfile,
-    report_callback callback
-) {
+void loop_cb(FILE *outfile,
+             report_callback callback)
+{
     for (int i = 0; i < 10; ++i) {
         callback(outfile, "Iteration %i\n", i);
     }
@@ -43,10 +42,9 @@ void dealloc_loop_iter(struct loop_iter *iter)
     // nothing
 }
 
-bool next_loop(
-    struct loop_iter *iter,
-    struct loop_result *res
-) {
+bool next_loop(struct loop_iter *iter,
+               struct loop_result *res)
+{
     // The loop is initialised (int i = 0) in the
     // iteration initialisation, but the loop
     // variable, i, should be incremented after
@@ -70,6 +68,200 @@ void loop_iteration(FILE *outfile)
         fprintf(outfile, "Iteration %d\n", result.i);
     }
     dealloc_loop_iter(&iter);
+}
+
+#pragma mark simple loop unwrapped
+
+void loop2(FILE *outfile)
+{
+    //init:
+    int i = 0;
+loop:
+    // report
+    fprintf(outfile, "Iteration %i\n", i);
+    // update frame
+    i++;
+    // next state
+    if (i < 10) goto loop;
+}
+
+enum loop2_labels { INIT, LOOP, DONE };
+struct loop2_iter {
+    // Execution pointer
+    enum loop2_labels state;
+    // Function call frame
+    int i;
+};
+
+bool next_loop2(struct loop2_iter *iter,
+                struct loop_result *res)
+{
+    while (true) {
+        switch(iter->state) {
+            case INIT:
+                iter->i = 0;
+                iter->state = LOOP;
+                break;
+
+            case LOOP:
+                // prepare report
+                res->i = iter->i;
+
+                // update frame
+                iter->i++;
+                // update state
+                if (!(iter->i < 10))
+                    iter->state = DONE;
+
+                // yield
+                return true;
+
+            case DONE:
+                return false;
+        }
+    }
+}
+
+void init_loop2_iter(struct loop2_iter *iter)
+{
+    iter->state = INIT;
+}
+void dealloc_loop2_iter(struct loop2_iter *iter)
+{
+    // nothing
+}
+
+void loop2_iteration(FILE *outfile)
+{
+    struct loop2_iter iter;
+    struct loop_result result;
+
+    init_loop2_iter(&iter);
+    while (next_loop2(&iter, &result)) {
+        fprintf(outfile, "Iteration %d\n", result.i);
+    }
+    dealloc_loop2_iter(&iter);
+}
+
+#pragma mark nested loops
+
+void nested_loop(FILE *outfile)
+{
+    for (int i = 0; i < 10; ++i) {
+        for (int j = 0; j < 10; ++j) {
+            fprintf(outfile, "Iteration %i x %i\n", i, j);
+        }
+    }
+}
+
+void nested_loop2(FILE *outfile)
+{
+    // frame
+    int i, j;
+
+    // init:
+    i = 0;
+
+outer_loop:
+    j = 0;
+
+inner_loop:
+    // report
+    fprintf(outfile, "Iteration %i x %i\n", i, j);
+    // update frame
+    j++;
+    // update state
+    if (j < 10) goto inner_loop;
+
+    // outer loop
+    // update frame
+    i++;
+    // update state
+    if (i < 10) goto outer_loop;
+
+}
+
+
+enum nested_loop_state {
+    NESTED_INIT,
+    NESTED_OUTER_LOOP,
+    NESTED_INNER_LOOP_PRE_REPORT,
+    NESTED_INNER_LOOP_POST_REPORT,
+    NESTED_DONE
+};
+struct nested_loop_iter {
+    enum nested_loop_state state;
+    int i;
+    int j;
+};
+struct nested_loop_result {
+    int i;
+    int j;
+};
+
+void init_nested_loop_iter(struct nested_loop_iter *iter)
+{
+    iter->state = NESTED_INIT;
+}
+void dealloc_nested_loop_iter(struct nested_loop_iter *iter)
+{
+    // nothing
+}
+
+bool next_nested_loop(struct nested_loop_iter *iter,
+                      struct nested_loop_result *res)
+{
+    while (true) {
+        switch(iter->state) {
+            case NESTED_INIT:
+                iter->i = 0;
+                iter->state = NESTED_OUTER_LOOP;
+                break;
+
+            case NESTED_OUTER_LOOP:
+                iter->j = 0;
+                iter->state = NESTED_INNER_LOOP_PRE_REPORT;
+                break;
+
+            case NESTED_INNER_LOOP_PRE_REPORT:
+                // Frame is already correct, so just report
+                // and update the execution state
+                res->i = iter->i;
+                res->j = iter->j;
+                iter->state = NESTED_INNER_LOOP_POST_REPORT;
+                // Yield
+                return true;
+
+            case NESTED_INNER_LOOP_POST_REPORT:
+                iter->j++;
+                if (iter->j < 10) {
+                    iter->state = NESTED_INNER_LOOP_PRE_REPORT;
+                    break;
+                }
+                iter->i++;
+                if (iter->i < 10) {
+                    iter->state = NESTED_OUTER_LOOP;
+                    break;
+                }
+                iter->state = NESTED_DONE;
+                break;
+
+            case NESTED_DONE:
+                return false;
+        }
+    }
+}
+
+void nested_loop_iteration(FILE *outfile)
+{
+    struct nested_loop_iter iter;
+    struct nested_loop_result result;
+    
+    init_nested_loop_iter(&iter);
+    while (next_nested_loop(&iter, &result)) {
+        fprintf(outfile, "Iteration %i x %i\n", result.i, result.j);
+    }
+    dealloc_nested_loop_iter(&iter);
 }
 
 int main(int argc, char **argv)
@@ -97,6 +289,19 @@ int main(int argc, char **argv)
         loop_cb(outfile, fprintf);
     } else if (strcmp(alg, "loop_iter") == 0) {
         loop_iteration(outfile);
+    } else if (strcmp(alg, "loop2") == 0) {
+        loop2(outfile);
+    } else if (strcmp(alg, "loop2_iter") == 0) {
+        loop2_iteration(outfile);
+
+    } else if (strcmp(alg, "nested_loop") == 0) {
+        nested_loop(outfile);
+    } else if (strcmp(alg, "nested_loop2") == 0) {
+        nested_loop2(outfile);
+    } else if (strcmp(alg, "nested_loop_iter") == 0) {
+        nested_loop_iteration(outfile);
+
+
     } else {
         fprintf(stderr, "Unknown algorithm %s\n", alg);
         fclose(outfile);
